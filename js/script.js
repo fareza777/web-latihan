ea629d9b207a61403210cbee06bdb619bd813f54
fareza777 <fajar.mreza@gmail.com>
2026-02-04 14:40:35 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 
+function toggleDetail(button) {
+  const detail = button.nextElementSibling;
+
+  if (detail.style.display === "none") {
+    detail.style.display = "block";
+    button.textContent = "Sembunyikan";
+  } else {
+    detail.style.display = "none";
+    button.textContent = "Lihat Detail";
+  }
+}
